cfbedbe68f6b00393c87eaabc97a7e8622514ab9
Williamsoditi <williamsoditi99@gmail.com>
2024-02-15 11:31:34 +0300

    Adds header component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import "./App.css";
+import Header from "./components/Header"
 import Todo from "./components/Todo"
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
       <Todo/>
     </div>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "./header.module.css";
+
+const Header = () => {
+    return ( 
+        <div className={styles.header}>
+            <h3>To-Do List</h3>
+        </div>
+     );
+}
+ 
+export default Header;
