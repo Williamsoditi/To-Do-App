844d2953b64d56b7b50c35625310257c9d81a287
Williamsoditi <williamsoditi99@gmail.com>
2024-02-15 10:35:04 +0300

    Feat : Functionality to submit to do items

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,12 +2,21 @@ import { useState } from "react";
 const Todo = () => {
 
     const [todo, setTodo] = useState("");
+    const [todos, settodos] = useState([]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        settodos([...todos, todo]);
+        setTodo("");
+    }
+
     return (  
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)}/>
-                <button type="submit">Add Task</button>
+                <button type="submit">Add</button>
             </form>
+            {console.log(todos)}
         </div>
     );
 }
