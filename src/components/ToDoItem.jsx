e4f0cabde5ffdc5beb32e1a48490c2dc429e8453
Williamsoditi <williamsoditi99@gmail.com>
2024-02-20 13:54:15 +0300

    Feat : Adds mark functionality to mark items once done with a strikethrough

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,4 +1,4 @@
-import "./toDoItem.css"
+import styles from "./toDoItem.module.css"
 
 const ToDoItem = ({ item, todos, settodos }) => {
     function handleDelete(item) {
@@ -7,18 +7,21 @@ const ToDoItem = ({ item, todos, settodos }) => {
     }
 
     function handleClick(name) {
-        console.log("Item click -" ,name)
+        const newArray = todos.map((todo) => todo.name === name ? { ...todo, done: !todo.done } : todo)
+        settodos(newArray)
     }
-    
-    return ( 
+
+    const strikeThrough = item.done ? styles.completed : ""
+
+    return (
         <div className=" row item">
-            <div className="col-sm-10 itemName"><span onClick={ ()=> {handleClick(item.name)}}>{item.name}</span></div>
+            <div className="col-sm-10 mt-3 fw-bold itemName"><h4 className={strikeThrough} onClick={() => { handleClick(item.name) }}>{item.name}</h4></div>
             <div className="col-sm-2">
-                <button className="btn btn-sm btn-danger px-4 mb-1 mt-3 deleteBtn" onClick={()=>handleDelete(item)}>X</button>
+                <button className="btn btn-sm btn-danger px-4 mb-1 mt-3 deleteBtn" onClick={() => handleDelete(item)}>X</button>
             </div>
-            <hr className="mt-3"/>
+            <hr className="mt-3" />
         </div>
-     );
+    );
 }
- 
+
 export default ToDoItem;
